f50d49bcb5b993437689b6918f46bc4d58050523
Vishal <vishalvarun27@gmail.com>
2024-03-09 12:26:03 +0530

    Made store accessible to frontend files.
    -Maincomp.js
    
    And then the data  is passed as parameter to
    the Slide.js file.

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -1,72 +1,102 @@
-import React from "react";
+// @ts-nocheck
+import React, { useEffect, useState } from "react";
 import "./cart.css";
 import { Divider } from "@mui/material";
+import { useParams } from "react-router-dom";
 
 const Cart = () => {
+  const { id } = useParams("");
+  //console.log(id);
+
+  const [indata, setInddata] = useState([]);
+  //console.log(indata);
+
+  const getindata = async () => {
+    const res = await fetch(`/getproductsone/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if (res.status != 202) {
+      console.log("no data available");
+    } else {
+      console.log("getdata");
+      setInddata(data);
+    }
+  };
+
+  useEffect(() => {
+    getindata();
+  }, [id]);
+
   return (
     <div className="cart_section">
-      <div className="cart_container">
-        <div className="left_cart">
-          <img
-            src="https://rukminim1.flixcart.com/image/416/416/kohigsw0/resistance-tube/c/s/e/new-adjustable-single-resistance-tube-multicolor-na-ajro-deal-original-imag2xg88mhmwxz5.jpeg?q=70"
-            alt="cart_img"
-          />
-          <div className="cart_btn">
-            <button className="cart_btn1">Add to Cart</button>
-            <button className="cart_btn2">Buy Now</button>
+      {indata && Object.keys(indata).length && (
+        <div className="cart_container">
+          <div className="left_cart">
+            <img src={indata.detailUrl} alt="cart_img" />
+            <div className="cart_btn">
+              <button className="cart_btn1">Add to Cart</button>
+              <button className="cart_btn2">Buy Now</button>
+            </div>
           </div>
-        </div>
-        <div className="right_cart">
-          <h3>Fitness Gear</h3>
-          <h4>
-            AJRO DEAL New Adjustable Single Resistance Tube (Multicolor)
-            Resistance Tube (Multicolor)
-          </h4>
-          <Divider />
-          <p className="mrp">M.R.P. : ₹1195</p>
-          <p>
-            Deal of the Day : <span style={{ color: "#B12704" }}>₹625.00</span>
-          </p>
-          <p>
-            You save : <span style={{ color: "#B12704" }}>₹570 (47%)</span>
-          </p>
-
-          <div className="discount_box">
-            <h5>
-              Discount : <span style={{ color: "#111" }}>Extra 10% Off</span>
-            </h5>
-            <h4>
-              Free Delivery{" "}
-              <span style={{ color: "#111", fontWeight: 600 }}>Oct 8 - 21</span>{" "}
-              Details{" "}
-            </h4>
+          <div className="right_cart">
+            <h3>{indata.title.shortTitle}</h3>
+            <h4>{indata.title.longTitle}</h4>
+            <Divider />
+            <p className="mrp">M.R.P. : ₹{indata.price.mrp}</p>
             <p>
-              Fastest delivery:{" "}
-              <span style={{ color: "#111", fontWeight: 600 }}>
-                Tomorrow 11AM
+              Deal of the Day :{" "}
+              <span style={{ color: "#B12704" }}>₹{indata.price.cost}.00</span>
+            </p>
+            <p>
+              You save :{" "}
+              <span style={{ color: "#B12704" }}>
+                ₹{indata.price.mrp - indata.price.cost} ({indata.price.discount}
+                )
+              </span>
+            </p>
+
+            <div className="discount_box">
+              <h5>
+                Discount :{" "}
+                <span style={{ color: "#111" }}>{indata.discount}</span>
+              </h5>
+              <h4>
+                Free Delivery{" "}
+                <span style={{ color: "#111", fontWeight: 600 }}>
+                  Oct 8 - 21
+                </span>{" "}
+                Details{" "}
+              </h4>
+              <p>
+                Fastest delivery:{" "}
+                <span style={{ color: "#111", fontWeight: 600 }}>
+                  Tomorrow 11AM
+                </span>
+              </p>
+            </div>
+            <p className="description">
+              About the Item :{" "}
+              <span
+                style={{
+                  color: "#565959",
+                  fontSize: 14,
+                  fontWeight: 500,
+                  letterSpacing: "0.4px",
+                }}
+              >
+                {indata.description}
               </span>
             </p>
           </div>
-          <p className="description">
-            About the Item :{" "}
-            <span
-              style={{
-                color: "#565959",
-                fontSize: 14,
-                fontWeight: 500,
-                letterSpacing: "0.4px",
-              }}
-            >
-              This unique product can tone your back muscles, reduce belly fat,
-              improve blood circulation and also improves your body posture. It
-              increases the stamina, energy and vitality of the body. The
-              elastic resistance of the rubber training rope can be used to
-              train and exercise in whichever way you want, according to your
-              physical needs.
-            </span>
-          </p>
         </div>
-      </div>
+      )}
     </div>
   );
 };
--- src/components/home/Slide.js
+++ src/components/home/Slide.js
@@ -2,8 +2,8 @@ import React from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { Divider } from '@mui/material';
-import { products } from './productdata';
 import "./slide.css";
+import {NavLink} from "react-router-dom";
 
 const responsive = {
     superLargeDesktop: {
@@ -24,7 +24,7 @@ const responsive = {
     }
   };
 
-const Slide = ({title}) => {
+const Slide = ({title,products}) => {
   return (
     <div className='products_section'>
         <div className="products_deal">
@@ -51,6 +51,7 @@ const Slide = ({title}) => {
             {
                 products.map((e)=>{
                     return (
+                      <NavLink to={`/getproductsone/${e.id}`}>
                         <div className="products_items">
                             <div className="product_img">
                                 <img src={e.url} alt="productitem" />
@@ -59,6 +60,7 @@ const Slide = ({title}) => {
                             <p className="products_offer">{e.discount}</p>
                             <p className='products_explore'>{e.tagline}</p>
                         </div>
+                        </NavLink>
                     )
                 })
             }
