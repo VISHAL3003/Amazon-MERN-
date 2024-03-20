3ba53cb8662f225ade6f078cc74979c280d1f95d
Vishal <vishalvarun27@gmail.com>
2024-03-20 17:48:42 +0530

    Made Buynow page interactive.

--- src/components/buynow/Subtotal.js
+++ src/components/buynow/Subtotal.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Subtotal = () => {
+const Subtotal = ({item}) => {
+
+  const [price,setPrice] = useState(0);
+
+  useEffect(()=>{
+    totalAmount();
+  },[item])
+
+  const totalAmount = () => {
+    let price = 0;
+    item.map((item)=>{
+      price += item.price.cost;
+    });
+    setPrice(price);
+  }
+
   return (
     <div className="sub_item">
       <h3>
-        Subtotal (1 items):{" "}
-        <strong style={{ color: "#111", fontWeight: 700 }}>₹4049.00</strong>
+        Subtotal ({item.length} items):{" "}
+        <strong style={{ color: "#111", fontWeight: 700 }}>₹{price}.00</strong>
       </h3>
     </div>
   );
